6adad6ce3c9bea1c7522d6b94a7b4934e9d09592
Shalindra Perera <129642367+Shalindra-CyberCodeX@users.noreply.github.com>
2023-04-06 15:56:18 +0530

    Update Stack using Linked list.cpp

--- Data_Structure_and_Algorithms/Lab 6/Stack using Linked list.cpp
+++ Data_Structure_and_Algorithms/Lab 6/Stack using Linked list.cpp
@@ -1,13 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-// C++ program to Implement a stack
-// using singly linked list
 #include <bits/stdc++.h>
 #include <ctime>
 using namespace std;
